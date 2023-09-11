aa8b0fa6c7404a12f16712d1817e5f821ee232ed
ingvilow <73110438+ingvilow@users.noreply.github.com>
2023-09-11 16:57:33 +0300

    fix(24): create mapper for ResultDTO and create ResultModel

--- lib/data/mappers/rick_and_morty_mapper.dart
+++ lib/data/mappers/rick_and_morty_mapper.dart
@@ -1,8 +1,17 @@
+import 'package:test_intern/data/dtos/result.dart';
 import 'package:test_intern/data/dtos/rick_and_morty.dart';
+import 'package:test_intern/domain/models/result_model.dart';
 import 'package:test_intern/domain/models/rick_and_morty_model.dart';
 
 extension RickAndMortyFromDTOToModel on RickMortyCharactersDto {
   RickAndMortyModel toDomain() {
-    return RickAndMortyModel(results: results, info: info);
+    final resultList = results.map((resultDto) => resultDto.toDomain()).toList();
+    return RickAndMortyModel(results: resultList, info: info);
+  }
+}
+
+extension ResultDTOToModel on ResultDto {
+  ResultModel toDomain() {
+    return ResultModel(id: id, name: name, image: image);
   }
 }
--- lib/domain/models/rick_and_morty_model.dart
+++ lib/domain/models/rick_and_morty_model.dart
@@ -1,8 +1,9 @@
 import 'package:test_intern/data/dtos/info.dart';
-import 'package:test_intern/data/dtos/result.dart';
+import 'package:test_intern/domain/models/result_model.dart';
 
 class RickAndMortyModel {
   final InfoDto? info;
-  final List<ResultDto>? results;
+  final List<ResultModel>? results;
+
   RickAndMortyModel({this.results, this.info});
 }
