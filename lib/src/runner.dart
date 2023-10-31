d3d0712c8915bddc81c6e79d5429ab3f2aebd766
radroot <radroot@gmail.com>
2023-10-31 17:09:30 +0400

    feat: generate firebase web config

--- lib/src/runner.dart
+++ lib/src/runner.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:test_intern/src/firebase_options.dart';
 import 'my_app.dart';
 
 void runner({required String appName}) async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   runApp(MyApp(appName: appName));
 }
