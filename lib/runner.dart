5c9f204c3796b3b10833c98af48c46118ad79b04
radroot <radroot@gmail.com>
2023-10-31 16:03:26 +0400

    feat: split app on app and runner

---
+++ lib/runner.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'my_app.dart';
+
+void runner() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  runApp(const MyApp());
+}
